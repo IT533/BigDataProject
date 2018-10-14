db5ce43fa8a13cdb066d590e3189ca67848bf029
Shuai-Hsun <hellovictorlee@gmail.com>
2018-10-14 13:33:46 -0700

    modify: top10

--- src/Top10.py
+++ src/Top10.py
@@ -4,26 +4,30 @@ Top 10 Views
 from mrjob.job import MRJob
 import heapq
 
+
 class MRTop10Views(MRJob):
-    # def mapper_init(self):
-    #     # MRJob.__init__(args = None)
-    #     self.view_lst = []
-    #     self.top_10_view = []
+    def mapper_init(self):
+        self.view_lst = []
 
     def mapper(self, _, line):
-        view_lst = []
         vID = line.split('\t')[0]
         view = line.split('\t')[5]
-        context = {'vID': vID, 'view': view}
-        view_lst.append(context)
-        top_10_lst = heapq.nlargest(10,view_lst,lambda s:s['view'])
-        yield ('', top_10_lst)
+        context = {'vID': vID, 'view': int(view)}
+        self.view_lst.append(context)
+        self.view_lst = heapq.nlargest(10, self.view_lst, lambda s: s['view'])
 
-    def reducer(self, key, top_10_lst):
-        top_10_view = []        
-        top_10_view = top_10_view + list(top_10_lst)
-        top_10_view = heapq.nlargest(10,top_10_view,lambda s:s['view'])
-        yield ('', top_10_view)
+    def mapper_final(self):
+        yield (None, self.view_lst)
+
+    def reducer(self, key, values):
+        top_10_view = []
+        for value in values:
+            for context in value:
+                top_10_view.append(context)
+                top_10_view = heapq.nlargest(10, top_10_view, lambda s: s['view'])
+
+        for value in top_10_view:
+            yield (key, value)
 
 
 if __name__ == '__main__':
