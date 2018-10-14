ea51906b0cbe614123470235aff2fa26a320c69a
Shuai-Hsun <hellovictorlee@gmail.com>
2018-10-13 23:48:41 -0700

    update

--- test/datascrub.py
+++ test/datascrub.py
@@ -1,3 +1,7 @@
+import os
+import re
+
+
 def scrub(fname):
     newfile = fname + '.new'
     with open(newfile, 'a') as fwrite:
@@ -8,5 +12,10 @@ def scrub(fname):
 
 
 if __name__ == '__main__':
-    fname = './input/0301/0.txt'
-    scrub(fname)
+    files_list = []
+    for root, dirs, files in os.walk('./input/'):
+        for f in files:
+            if re.match('[0-9].txt', f):
+                files_list.append(root + '/' + f)
+    for fname in files_list:
+        scrub(fname)
--- test/mergefile.py
+++ test/mergefile.py
@@ -4,12 +4,13 @@ import re
 files_list = []
 for root, dirs, files in os.walk('./input/'):
     for f in files:
-        if re.match('[0-9].txt', f):
-            files_list.append(root + '/' + f)
+        if re.match('[0-9]+.txt.new', f):
+            files_list.append(f)
+
 
 for fpath in files_list:
-    with open('./input/allfile.txt', 'a') as fwrite:
-        with open(fpath, 'r') as fread:
+    with open('./input/allfile.txt.new', 'a') as fwrite:
+        with open('./input/' + fpath, 'r') as fread:
             fwrite.write(fread.read())
         fread.close()
     fwrite.close()
